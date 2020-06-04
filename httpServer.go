ea18f2f49ef34535a96775b037a763b0f788f8ab
Ovy95 <jackoverton21@hotmail.com>
2020-06-04 08:46:27 +0100

    commandline message to confirm server is stared when run

--- httpServer.go
+++ httpServer.go
@@ -5,10 +5,11 @@ import (
 	"net/http"
 )
 
-func index_handler(w http.ResponseWriter, r *http.Request) {
+func indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Test text to appear on page")
 }
 func main() {
-	http.HandleFunc("/", index_handler)
+	http.HandleFunc("/", indexHandler)
+	fmt.Println("Server started on port 8080")
 	http.ListenAndServe(":8080", nil)
 }
