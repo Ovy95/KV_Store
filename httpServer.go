d9b46a5be889da3d33b7af894de88e4edff39f55
Ovy95 <jackoverton21@hotmail.com>
2020-06-05 15:36:30 +0100

    Case method updated

--- httpServer.go
+++ httpServer.go
@@ -16,15 +16,14 @@ func main() {
 }
 func handler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
-	case get:
-		fmt.Fprintf(w, "CASE METHOD   ")
-		fmt.Fprintf(w, "")
-		fmt.Fprintf(w, "THIS IS A %s method ON THE LOCALHOST AT THE INDEX%s\n", r.Method, r.URL)
-		fmt.Println(r.Method)
 	case post:
 		fmt.Fprintf(w, "THIS IS A %s method ON THE LOCALHOST AT THE INDEX%s\n", r.Method, r.URL)
 		fmt.Println(r.Method)
 	case delete:
+		fmt.Fprintf(w, "THIS IS A %s method ON THE LOCALHOST AT THE INDEX%s\n", r.Method, r.URL)
+		fmt.Println(r.Method)
+	case get:
+		fmt.Fprintf(w, "THIS IS A %s method ON THE LOCALHOST AT THE INDEX%s\n", r.Method, r.URL)
 		fmt.Println(r.Method)
 	}
 	// fmt.Println(r.Method)
