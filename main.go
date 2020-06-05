96f56bee96f44a0ea84a69fb3490d6d0695f0c28
Ovy95 <jackoverton21@hotmail.com>
2020-06-05 16:24:59 +0100

    Errors added, + little refactors

--- main.go
+++ main.go
@@ -2,29 +2,39 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
-const get string = "GET"
-const post string = "POST"
-const delete string = "DELETE"
+const get = "GET"
+const post = "POST"
+const delete = "DELETE"
 
 func main() {
 	http.HandleFunc("/", handler)
 	fmt.Println("Server is starting on port 8080")
-	http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
 func handler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case post:
 		fmt.Fprintf(w, "THIS IS A %s method ON THE LOCALHOST AT THE INDEX%s\n", r.Method, r.URL)
 		fmt.Println(r.Method)
+		break
 	case delete:
 		fmt.Fprintf(w, "THIS IS A %s method ON THE LOCALHOST AT THE INDEX%s\n", r.Method, r.URL)
 		fmt.Println(r.Method)
+		break
 	case get:
 		fmt.Fprintf(w, "THIS IS A %s method ON THE LOCALHOST AT THE INDEX%s\n", r.Method, r.URL)
 		fmt.Println(r.Method)
+		break
+	default:
+		http.Error(w, "BadRequest status code", 400)
+		//log the errors default case on the switch statement to log and error and return a BadRequest status code
 	}
 	// fmt.Println(r.Method)
 }
