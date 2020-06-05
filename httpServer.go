153ac924ab24520a5c42a06d1a40d58ea6ee0ca3
Ovy95 <jackoverton21@hotmail.com>
2020-06-05 13:56:39 +0100

    Prints out what the method does on the page

--- httpServer.go
+++ httpServer.go
@@ -5,11 +5,13 @@ import (
 	"net/http"
 )
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Test text to appear on page")
-}
 func main() {
-	http.HandleFunc("/", indexHandler)
-	fmt.Println("Server started on port 8080")
+	http.HandleFunc("/", handler)
+	fmt.Println("Server is starting on port 8080")
 	http.ListenAndServe(":8080", nil)
+}
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Test text to appear on page    ")
+	fmt.Fprintf(w, "")
+	fmt.Fprintf(w, "%s is the method. This is the URL %s\n", r.Method, r.URL)
 }
