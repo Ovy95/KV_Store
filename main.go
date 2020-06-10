977307535d941cfaacfd082aa399ed68ae52b3ea
Ovy95 <jackoverton21@hotmail.com>
2020-06-10 08:56:20 +0100

    added case

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 )
 
+const set = "SET"
 const get = "GET"
 const post = "POST"
 const delete = "DELETE"
@@ -18,23 +19,37 @@ func main() {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
+	case get:
+		fmt.Fprintf(w, "THIS IS A %s method ON THE LOCALHOST AT THE INDEX%s\n", r.Method, r.URL)
+		break
+	case set:
+		fmt.Fprintf(w, "THIS IS A %s method ON THE LOCALHOST AT THE INDEX%s\n", r.Method, r.URL)
+		fmt.Println(r.Method)
+		fmt.Fprintf(w, "Delete")
+		break
 	case post:
 		fmt.Fprintf(w, "THIS IS A %s method ON THE LOCALHOST AT THE INDEX%s\n", r.Method, r.URL)
 		fmt.Println(r.Method)
+		fmt.Fprintf(w, "Post")
 		break
 	case delete:
 		fmt.Fprintf(w, "THIS IS A %s method ON THE LOCALHOST AT THE INDEX%s\n", r.Method, r.URL)
 		fmt.Println(r.Method)
-		break
-	case get:
-		fmt.Fprintf(w, "THIS IS A %s method ON THE LOCALHOST AT THE INDEX%s\n", r.Method, r.URL)
-		fmt.Println(r.Method)
+		fmt.Fprintf(w, "Delete")
 		break
 	default:
 		http.Error(w, "BadRequest status code", 400)
-		//log the errors default case on the switch statement to log and error and return a BadRequest status code
 	}
-	// fmt.Println(r.Method)
+}
+
+func storeInterface() {
+	The store interface should support the following operations:
+	- Get(key string) []byte : This will return the value for the given key
+	  or will return nil if it doesn’t exist. Your http handler should return
+	  a NOT FOUND status code if that is the case
+	- Set(key string, value []byte): this will set or update the key with the specified value
+	- Delete(key string): this will delete the key from the store
 }
