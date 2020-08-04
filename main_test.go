04c265eec5483a4d5b35bea544ce22a512deb3c4
Ovy95 <jackoverton21@hotmail.com>
2020-08-04 14:54:14 +0100

    Updated first text to display error message also Status code has changed

--- main_test.go
+++ main_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestHealthCheckHandler(t *testing.T) {
 
-	req, err := http.NewRequest("GET", "/", nil)
+	req, err := http.NewRequest("GET", "/string", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -19,47 +19,91 @@ func TestHealthCheckHandler(t *testing.T) {
 
 	handler.ServeHTTP(rr, req)
 
-	if status := rr.Code; status != http.StatusOK {
+	if status := rr.Code; status != http.StatusNotFound {
 		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
+			status, http.StatusNotFound)
 	}
 
-}
-
-func PostCheckHandler(t *testing.T) {
-
-	req, err := http.NewRequest("POST", "/hello", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	rr := httptest.NewRecorder()
-	KvHandlers := newHandlers()
-	handler := http.HandlerFunc(KvHandlers.SortData)
-
-	handler.ServeHTTP(rr, req)
-
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
-	}
-
-	get, err := http.NewRequest("GET", "/hello", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	getRR := httptest.NewRecorder()
-	handler.ServeHTTP(getRR, get)
-
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
-	}
-	expected := "hello"
-	if getRR.Body.String() != expected {
+	expected := "No Key could be found, try http://localhost:8080/ to see the list of stored keys"
+	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			rr.Body.String(), expected)
 	}
-
 }
+
+// Check tests are passing for the right reason !
+
+// func PostCheckHandler(t *testing.T) {
+
+// 	req, err := http.NewRequest("POST", "/hello", nil)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+
+// 	rr := httptest.NewRecorder()
+// 	KvHandlers := newHandlers()
+// 	handler := http.HandlerFunc(KvHandlers.SortData)
+
+// 	handler.ServeHTTP(rr, req)
+
+// 	if status := rr.Code; status != http.StatusOK {
+// 		t.Errorf("handler returned wrong status code: got %v want %v",
+// 			status, http.StatusOK)
+// 	}
+
+// 	get, err := http.NewRequest("GET", "/hello", nil)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+
+// 	getRR := httptest.NewRecorder()
+// 	handler.ServeHTTP(getRR, get)
+
+// 	if status := rr.Code; status != http.StatusOK {
+// 		t.Errorf("handler returned wrong status code: got %v want %v",
+// 			status, http.StatusOK)
+// 	}
+// 	expected := ""
+// 	if getRR.Body.String() != expected {
+// 		t.Errorf("handler returned unexpected body: got %v want %v",
+// 			rr.Body.String(), expected)
+// 	}
+// }
+
+// func PostPostGetCheckHandler(t *testing.T) {
+
+// 	req, err := http.NewRequest("POST", "/hello", nil)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	req2, err := http.NewRequest("POST", "/world", nil)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+
+// 	postRR := httptest.NewRecorder()
+
+// 	KvHandlers := newHandlers()
+// 	handler := http.HandlerFunc(KvHandlers.SortData)
+
+// 	handler.ServeHTTP(postRR, req)
+// 	handler.ServeHTTP(postRR, req2)
+
+// 	getreq, err := http.NewRequest("GET", "/", nil)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+
+// 	getRR := httptest.NewRecorder()
+// 	handler.ServeHTTP(getRR, getreq)
+
+// 	if status := getRR.Code; status != http.StatusOK {
+// 		t.Errorf("handler returned wrong status code: got %v want %v",
+// 			status, http.StatusOK)
+// 	}
+// 	expected := ""
+// 	if getRR.Body.String() != expected {
+// 		t.Errorf("handler returned unexpected body: got %v want %v",
+// 			getRR.Body.String(), expected)
+// 	}
+// }
