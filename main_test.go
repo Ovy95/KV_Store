57f40a3055cdb62f1b64fbb96bdca4ed4b9fb3ad
Ovy95 <jackoverton21@hotmail.com>
2020-08-04 14:01:54 +0100

    Post test added, prints key to body and is checked on Get function

--- main_test.go
+++ main_test.go
@@ -25,3 +25,41 @@ func TestHealthCheckHandler(t *testing.T) {
 	}
 
 }
+
+func PostCheckHandler(t *testing.T) {
+
+	req, err := http.NewRequest("POST", "/hello", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	KvHandlers := newHandlers()
+	handler := http.HandlerFunc(KvHandlers.SortData)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	get, err := http.NewRequest("GET", "/hello", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	getRR := httptest.NewRecorder()
+	handler.ServeHTTP(getRR, get)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+	expected := "hello"
+	if getRR.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+
+}
