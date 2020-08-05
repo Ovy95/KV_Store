63f88ad1077e22c127edfe3373159ceea4d8dc51
Ovy95 <jackoverton21@hotmail.com>
2020-08-05 22:08:54 +0100

    Sortdata Function tested

--- main_test.go
+++ main_test.go
@@ -94,3 +94,79 @@ func TestPost(t *testing.T) {
 		t.Errorf("handler returned unexpected body: got%v want%v", getRR.Body.String(), expected)
 	}
 }
+
+func TestDeletePost(t *testing.T) {
+	bodyReader := strings.NewReader("Body Testing")
+
+	req, err := http.NewRequest("POST", "/hello", bodyReader)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	KvHandlers := newHandlers()
+	handler := http.HandlerFunc(KvHandlers.SortData)
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	get, err := http.NewRequest("GET", "/hello", http.NoBody)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	getRR := httptest.NewRecorder()
+	handler.ServeHTTP(getRR, get)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expectedBT := "Body Testing"
+	if getRR.Body.String() != expectedBT {
+		t.Errorf("handler returned unexpected body: got%v want%v", getRR.Body.String(), expectedBT)
+	}
+
+	delete, err := http.NewRequest("DELETE", "/hello", http.NoBody)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	deleteRR := httptest.NewRecorder()
+	handler.ServeHTTP(deleteRR, delete)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expectedDeleted := ""
+	if deleteRR.Body.String() != expectedDeleted {
+		t.Errorf("handler returned unexpected body: got%v want%v", deleteRR.Body.String(), expectedDeleted)
+	}
+	/////
+	req, err = http.NewRequest("GET", "/hello", http.NoBody)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rrG := httptest.NewRecorder()
+	handler.ServeHTTP(rrG, req)
+
+	if status := rrG.Code; status != http.StatusNotFound {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusNotFound)
+	}
+
+	expected := "No Key could be found, try http://localhost:8080/ to see the list of stored keys"
+	if rrG.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rrG.Body.String(), expected)
+	}
+
+}
